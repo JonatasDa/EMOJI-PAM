22a70d5b8cead9ffa203e8b7e916fd2f920172e3
jonnycebola <47872499+JonatasDa@users.noreply.github.com>
2025-08-11 10:51:16 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const select = document.querySelector('#select');
+const enviar = document.querySelector('#enviar');
+const resultado = document.querySelector('#emojiGallery');
+
+enviar.addEventListener('click', function(){😎😢😘
+    const span = document.createElement('span');
+
+    if(select == v1){
+        span.textContent = "😂";
+        .appendChild(span);
+        res
+    }
+})
