97efd367ae2a1d528c21aa78b0f7db58fda30162
jonnycebola <47872499+JonatasDa@users.noreply.github.com>
2025-08-13 09:31:20 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 const select = document.querySelector('#select');
 const enviar = document.querySelector('#enviar');
 const resultado = document.querySelector('#emojiGallery');
+const limpar = document.querySelector("#limpar");
 
-enviar.addEventListener('click', function(){😎😢😘
+enviar.addEventListener('click', function(){
     const span = document.createElement('span');
 
-    if(select == v1){
-        span.textContent = "😂";
-        .appendChild(span);
-        res
-    }
+        span.textContent = select.options[select.selectedIndex].text;
+
+        resultado.appendChild(span);
+        span.classList.add('emoji');
+})
+
+limpar.addEventListener('click', function(){
+    resultado.innerHTML = "";
 })
